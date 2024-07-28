76f4497c9518169c7db40b88c3a7347890f0bbb4
Purushottam Sonar <sonarp02@gmail.com>
2024-07-29 03:49:25 +0530

    login updated

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,10 +17,19 @@ const registerUser = async (req, res) => {
   }
 
   try {
-    const userExists = await User.findOne({ email });
+    const userExistsByUsername = await User.findOne({ username });
+    if (userExistsByUsername) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
 
-    if (userExists) {
-      return res.status(400).json({ message: "User already exists" });
+    const userExistsByEmail = await User.findOne({ email });
+    if (userExistsByEmail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const userExistsByMobile = await User.findOne({ mobile });
+    if (userExistsByMobile) {
+      return res.status(400).json({ message: "Mobile number already exists" });
     }
 
     let profilePic = null;
@@ -67,113 +76,39 @@ const registerUser = async (req, res) => {
     res.status(500).json({ message: error.message, success: false });
   }
 };
-const authUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-
-    if (user && (await user.matchPassword(password))) {
-      res.json({
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-        mobile: user.mobile,
-        role: user.role,
-        profilePic: user.profilePic,
-        token: generateToken(user._id), // Generate token upon successful login
-      });
-    } else {
-      res.status(401).json({ message: "Invalid email or password" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-// Get user profile
-const getUserProfile = async (req, res) => {
-  try {
-    const user = await User.findById(req.user._id);
-
-    if (user) {
-      res.json({
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-        mobile: user.mobile,
-        role: user.role,
-        profilePic: user.profilePic,
-      });
-    } else {
-      res.status(404).json({ message: "User not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-// const updateUserProfile = async (req, res) => {
-//   try {
-//     const user = await User.findById(req.user._id);
-
-//     if (user) {
-//       user.username = req.body.username || user.username;
-//       user.email = req.body.email || user.email;
-//       user.mobile = req.body.mobile || user.mobile;
-
-//       if (req.body.password) {
-//         user.password = req.body.password;
-//       }
-
-//       if (req.file) {
-//         // Destroy the old profile pic if it exists
-//         if (user.profilePicPublicId) {
-//           await cloudinary.v2.uploader.destroy(user.profilePicPublicId);
-//         }
-
-//         // Convert the file buffer to a base64 string
-//         const b64 = Buffer.from(req.file.buffer).toString("base64");
-//         const dataURI = `data:${req.file.mimetype};base64,${b64}`;
-
-//         // Upload the new profile pic to Cloudinary
-//         const cldRes = await handleUpload(dataURI, "user_profiles");
-
-//         // Update user profile pic details
-//         user.profilePic = cldRes.secure_url;
-//         user.profilePicPublicId = cldRes.public_id;
-//       }
-
-//       // Save the updated user
-//       const updatedUser = await user.save();
-
-//       res.json({
-//         _id: updatedUser._id,
-//         username: updatedUser.username,
-//         email: updatedUser.email,
-//         mobile: updatedUser.mobile,
-//         role: updatedUser.role,
-//         profilePic: updatedUser.profilePic,
-//         token: generateToken(updatedUser._id), // Generate token upon profile update
-//       });
-//     } else {
-//       res.status(404).json({ message: "User not found" });
-//     }
-//   } catch (error) {
-//     console.error("Error updating profile:", error);
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-// Delete user
-// Update user profile
 const updateUserProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user._id);
 
     if (user) {
-      user.username = req.body.username || user.username;
-      user.email = req.body.email || user.email;
-      user.mobile = req.body.mobile || user.mobile;
+      const { username, email, mobile } = req.body;
+
+      // Check for existing username, email, and mobile number
+      if (username && username !== user.username) {
+        const userExistsByUsername = await User.findOne({ username });
+        if (userExistsByUsername) {
+          return res.status(400).json({ message: "Username already exists" });
+        }
+        user.username = username;
+      }
+
+      if (email && email !== user.email) {
+        const userExistsByEmail = await User.findOne({ email });
+        if (userExistsByEmail) {
+          return res.status(400).json({ message: "Email already exists" });
+        }
+        user.email = email;
+      }
+
+      if (mobile && mobile !== user.mobile) {
+        const userExistsByMobile = await User.findOne({ mobile });
+        if (userExistsByMobile) {
+          return res
+            .status(400)
+            .json({ message: "Mobile number already exists" });
+        }
+        user.mobile = mobile;
+      }
 
       if (req.body.password) {
         user.password = req.body.password;
@@ -221,6 +156,57 @@ const updateUserProfile = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+const authUser = async (req, res) => {
+  const { emailOrMobile, password } = req.body;
+
+  try {
+    // Determine if the input is an email or a mobile number
+    const user = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailOrMobile)
+      ? await User.findOne({ email: emailOrMobile })
+      : await User.findOne({ mobile: emailOrMobile });
+
+    if (user && (await user.matchPassword(password))) {
+      res.status(201).json({
+        message: "User login successfully!",
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        mobile: user.mobile,
+        role: user.role,
+        profilePic: user.profilePic,
+        token: generateToken(user._id), // Generate token upon successful login
+      });
+    } else {
+      res
+        .status(401)
+        .json({ message: "Invalid email, mobile number, or password" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+      res.json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        mobile: user.mobile,
+        role: user.role,
+        profilePic: user.profilePic,
+      });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 const deleteUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
@@ -235,11 +221,10 @@ const deleteUser = async (req, res) => {
       res.status(404).json({ message: "User not found" });
     }
   } catch (error) {
-    res.status({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
-// Get all users
 const getUsers = async (req, res) => {
   try {
     const users = await User.find({});
@@ -249,7 +234,6 @@ const getUsers = async (req, res) => {
   }
 };
 
-// Get user by ID
 const getUserById = async (req, res) => {
   try {
     const user = await User.findById(req.params.id).select("-password");
@@ -262,7 +246,6 @@ const getUserById = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-
 export {
   registerUser,
   authUser,
